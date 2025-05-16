6cf69b768ac2a7cf443602e218e801bea750d7d7
Tom Seimandi <tom.seimandi@finances.gouv.fr>
2025-05-16 15:54:09 +0200

    Improve ml ranker code

--- src/rank_comparia/maximum_likelihood.py
+++ src/rank_comparia/maximum_likelihood.py
@@ -27,90 +27,57 @@ class MaximumLikelihoodRanker(Ranker):
     @staticmethod
     def aggregate_matches(matches: Iterable[Match]) -> pl.DataFrame:
         # store match list in dataframe
-        model_a = []
-        model_b = []
-        a_wins = []  # a wins
-        b_wins = []
-        draws = []
-        for m in matches:
-            model_a.append(m.model_a)
-            model_b.append(m.model_b)
-            a_wins.append(int(m.score.value / 2))
-            b_wins.append(max(0, 1 - m.score.value))
-            draws.append(1 - int(m.score.value / 2) - max(0, 1 - m.score.value))
-        df = pl.DataFrame(
-            {"model_a_name": model_a, "model_b_name": model_b, "a_wins": a_wins, "b_wins": b_wins, "draws": draws}
+        model_a, model_b, score = zip(*((match.model_a, match.model_b, match.score.value) for match in matches))
+        df = (
+            pl.DataFrame({"model_a_name": model_a, "model_b_name": model_b, "score": score})
+            .with_columns(
+                a_wins=(pl.col("score") / 2).cast(int),
+                b_wins=pl.max_horizontal(0, 1 - pl.col("score")),
+            )
+            .with_columns(draws=1 - pl.col("a_wins") - pl.col("b_wins"))
+            .drop("score")
+            .filter(pl.col("model_a_name") != pl.col("model_b_name"))
         )
-
-        # filter out matches with identical a and b models
-        df = df.filter(pl.col("model_a_name") != pl.col("model_b_name"))
 
         # aggregate match results
-        counts = df.group_by(["model_a_name", "model_b_name"]).agg(
-            pl.col("a_wins").sum(), pl.col("b_wins").sum(), pl.col("draws").sum()
+        counts = df.group_by(["model_a_name", "model_b_name"]).sum()
+        reversed_counts = counts.with_columns(
+            model_a_name="model_b_name",
+            model_b_name="model_a_name",
+            a_wins="b_wins",
+            b_wins="a_wins",
         )
-        reversed_counts = (
-            df.group_by(["model_a_name", "model_b_name"])
-            .agg(pl.col("a_wins").sum(), pl.col("b_wins").sum(), pl.col("draws").sum())
-            .select(
-                pl.col("model_b_name").alias("model_a_name"),
-                pl.col("model_a_name").alias("model_b_name"),
-                pl.col("b_wins").alias("a_wins"),
-                pl.col("a_wins").alias("b_wins"),
-                pl.col("draws"),
-            )
-        )
-        all_counts = (
-            pl.concat([counts, reversed_counts])
-            .group_by(["model_a_name", "model_b_name"])
-            .agg(pl.sum("a_wins"), pl.sum("b_wins"), pl.sum("draws"))
-        )
+
+        all_counts = pl.concat([counts, reversed_counts]).group_by(["model_a_name", "model_b_name"]).sum()
         return all_counts
 
     def compute_scores(self, matches: Iterable[Match]) -> None:
         all_counts = self.aggregate_matches(matches=matches)
         # models list
         models = all_counts["model_a_name"].unique().to_list()
-        models_df = pl.DataFrame({"idx": np.arange(len(models)), "model": models})
-
-        # initialize matrices
-        p = len(models)
-        X = np.zeros([p * (p - 1) * 2, p])
-        Y = np.zeros(p * (p - 1) * 2)
-        cur_row = 0
+        # set up matrices to train model
+        non_mirror_matches = all_counts.filter(pl.col("model_a_name") != pl.col("model_b_name"))
+        X = np.zeros([len(non_mirror_matches) * 2, len(models)])
+        Y = np.zeros(len(non_mirror_matches) * 2)
         sample_weights = []
+        model_dict = {model_name: index for index, model_name in enumerate(models)}
+        base_log = math.log(self.BASE)
 
-        for r1 in models_df.rows(named=True):
-            model_a_idx = r1["idx"]
-            model_a_name = r1["model"]
-            for r2 in models_df.rows(named=True):
-                model_b_idx = r2["idx"]
-                model_b_name = r2["model"]
+        for current_index, (match_data) in enumerate(non_mirror_matches.iter_rows(named=True)):
+            model_a_idx, model_b_idx = model_dict[match_data["model_a_name"]], model_dict[match_data["model_b_name"]]
+            a_wins, b_wins, draws = match_data["a_wins"], match_data["b_wins"], match_data["draws"]
 
-                # skip case where models are identical
-                if model_a_idx == model_b_idx:
-                    continue
-                # if no row or no matches
-                match_data = all_counts.filter(
-                    pl.col("model_a_name") == model_a_name,
-                    pl.col("model_b_name") == model_b_name,
-                )
-                if len(match_data) == 0:
-                    continue
-                X[cur_row, model_a_idx] = math.log(self.BASE)
-                X[cur_row, model_b_idx] = -math.log(self.BASE)
-                Y[cur_row] = 1.0
-                sample_weights.append(match_data.row(0)[2] * 2 + match_data.row(0)[3])
+            X[2 * current_index, model_a_idx] = base_log
+            X[2 * current_index, model_b_idx] = -base_log
+            Y[2 * current_index] = 1.0
+            sample_weights.append(a_wins * 2 + draws)
 
-                X[cur_row + 1, model_a_idx] = math.log(self.BASE)
-                X[cur_row + 1, model_b_idx] = -math.log(self.BASE)
-                Y[cur_row + 1] = 0.0
-                sample_weights.append(match_data.row(0)[4] * 2 + match_data.row(0)[3])
-                cur_row += 2
+            X[2 * current_index + 1, model_a_idx] = base_log
+            X[2 * current_index + 1, model_b_idx] = -base_log
+            Y[2 * current_index + 1] = 0.0
+            sample_weights.append(b_wins * 2 + draws)
 
-        X = X[:cur_row]
-        Y = Y[:cur_row]
-
+        # fit logistic regression
         lr = LogisticRegression(fit_intercept=False, penalty=None, tol=1e-6, max_iter=self.max_iter)  # type: ignore
         lr.fit(X, Y, sample_weight=sample_weights)
         scores = self.scale * lr.coef_[0] + self.INIT_RATING
